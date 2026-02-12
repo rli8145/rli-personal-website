1c37772a0f43231e2e5d267f5176f008b62f7594
rli8145 <rli8145@gmail.com>
2026-02-12 15:42:25 -0500

    changed font, reduced spacing

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -28,7 +28,7 @@ function Resume() {
           {viewMode === 'download' ? (
             <div className="resume-download">
               <div className="download-card">
-                <svg width="64" height="64" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+                <svg width="40" height="40" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
                   <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/>
                   <polyline points="14 2 14 8 20 8"/>
                   <line x1="12" y1="18" x2="12" y2="12"/>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,18 @@
 import Hero from '../components/Hero'
 import About from '../components/About'
+import './HomePage.css'
 
 function HomePage() {
   return (
     <div className="page home-page">
-      <Hero />
-      <About />
+      <div className="home-container">
+        <div className="home-left">
+          <Hero />
+        </div>
+        <div className="home-right">
+          <About />
+        </div>
+      </div>
     </div>
   )
 }
