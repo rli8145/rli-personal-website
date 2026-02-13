64875920e16d00d5b4d2e0cafab52a2a387b34f6
rli8145 <rli8145@gmail.com>
2026-02-13 14:24:40 -0800

    minor edits - footer order, link font

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,10 +11,10 @@ function About() {
             and bringing innovative ideas to life. I enjoy:
           </p>
           <ul className="interests-list">
-            <li>mulling over puzzles and hard math problems</li>
+            <li>mulling over puzzles and HARD math problems</li>
             <li>building stuff!</li>
             <li>playing poker</li>
-            <li>good movies (<a href="https://letterboxd.com/rli8145/" target="_blank" rel="noopener noreferrer" className="external-link">Letterboxd<svg width="10" height="10" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><line x1="7" y1="17" x2="17" y2="7"/><polyline points="7 7 17 7 17 17"/></svg></a>) and music (<a href="https://open.spotify.com/user/ryanli-nz" target="_blank" rel="noopener noreferrer" className="external-link">Spotify<svg width="10" height="10" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><line x1="7" y1="17" x2="17" y2="7"/><polyline points="7 7 17 7 17 17"/></svg></a>)</li>
+            <li>good movies (<a href="https://letterboxd.com/rli8145/" target="_blank" rel="noopener noreferrer" className="external-link">Letterboxd<svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><line x1="7" y1="17" x2="17" y2="7"/><polyline points="7 7 17 7 17 17"/></svg></a>) and music (<a href="https://open.spotify.com/user/ryanli-nz" target="_blank" rel="noopener noreferrer" className="external-link">Spotify<svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"><line x1="7" y1="17" x2="17" y2="7"/><polyline points="7 7 17 7 17 17"/></svg></a>)</li>
             <li>playing golf</li>
           </ul>
           <p>
