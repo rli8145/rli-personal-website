3b0f590d6df5ab1a6716f4fa2fae18052ba79818
rli8145 <rli8145@gmail.com>
2026-02-13 17:05:57 -0800

    removed contact info from footer, small font size changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,6 @@ function Footer() {
     <footer className="footer">
       <div className="container">
         <div className="footer-content">
-          <div className="footer-links">
-            <a href="mailto:rli8145@gmail.com">Email</a>
-            <a href="https://github.com/rli8145" target="_blank" rel="noopener noreferrer">GitHub</a>
-            <a href="https://linkedin.com/in/ryan-li-0bab58363/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          </div>
           <p className="copyright">
             &copy; {currentYear} Ryan Li. All rights reserved.
           </p>
