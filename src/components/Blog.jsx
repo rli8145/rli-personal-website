accbffb977de9026951d877e37e90c2f20852da6
rli8145 <rli8145@gmail.com>
2026-02-13 16:09:05 -0800

    blog and header/footer transition

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,31 +1,12 @@
 import './Blog.css'
 
 function Blog() {
-  const posts = [
-    {
-      id: 1,
-      title: 'Coming Soon',
-      date: '2025',
-      excerpt: 'Blog posts will be added here soon. Stay tuned for updates on my projects, learnings, and thoughts on software development.',
-      slug: '#'
-    }
-  ]
-
   return (
     <section className="blog">
       <div className="container">
         <h2 className="section-title">Blog</h2>
-        <div className="blog-grid">
-          {posts.map((post) => (
-            <article key={post.id} className="blog-card">
-              <div className="blog-meta">
-                <span className="blog-date">{post.date}</span>
-              </div>
-              <h3>{post.title}</h3>
-              <p>{post.excerpt}</p>
-              <a href={post.slug} className="blog-link">Read more</a>
-            </article>
-          ))}
+        <div className="blog-list">
+          <p className="coming-soon">I'll post here about what I'm learning and working on - projects, internships (🙏), design teams, math, school, and more. Stay tuned!</p>
         </div>
       </div>
     </section>
