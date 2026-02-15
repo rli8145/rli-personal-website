b587aee5c9bc2e9458296fb860612ad104f5cfb5
rli8145 <rli8145@gmail.com>
2026-02-14 23:33:47 -0800

    added site analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import MainLayout from './layouts/MainLayout'
 import HomePage from './pages/HomePage'
 import ProjectsPage from './pages/ProjectsPage'
@@ -7,13 +8,16 @@ import './App.css'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="projects" element={<ProjectsPage />} />
-        <Route path="blog" element={<BlogPage />} />
-      </Route>
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="projects" element={<ProjectsPage />} />
+          <Route path="blog" element={<BlogPage />} />
+        </Route>
+      </Routes>
+      <Analytics />
+    </>
   )
 }
 
