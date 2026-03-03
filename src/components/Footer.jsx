65f92309ca1f04fec7bcbbec9829efb8203cde35
rli8145 <rli8145@gmail.com>
2026-03-03 14:51:49 -0500

    commented time update feature, update license

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,16 @@
 import './Footer.css'
 
+// const buildTime = new Date(__BUILD_TIME__).toLocaleString('en-US', {
+//   timeZone: 'America/New_York',
+//   year: 'numeric',
+//   month: 'short',
+//   day: 'numeric',
+//   hour: '2-digit',
+//   minute: '2-digit',
+//   second: '2-digit',
+//   timeZoneName: 'short',
+// })
+
 function Footer() {
   const currentYear = new Date().getFullYear()
 
@@ -8,7 +19,8 @@ function Footer() {
       <div className="container">
         <div className="footer-content">
           <p className="copyright">
-            &copy; {currentYear} Ryan Li. All rights reserved.
+            &copy; {currentYear} Ryan Li. Licensed under MIT.
+            {/* &nbsp;&middot;&nbsp; Last updated: {buildTime} */}
           </p>
         </div>
       </div>
